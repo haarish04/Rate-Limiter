1b3c502a1539cab32a8de956c32a136fdc83fb9b
haarish04 <haarish.anandan@gmail.com>
2024-05-19 12:44:02 +0530

    Refactored main to handle all algos, included comments in tokenBucket

--- src/TokenBucket.js
+++ src/TokenBucket.js
@@ -7,9 +7,13 @@ function TokenBucket(rate, capacity) {
     const deltaTime = now - lastCheck;
     lastCheck = now;
 
+    //deltatime will give the secs elapsed * rate will give the number of tokens to be replenished, divided by 1000 as 1000 is one sec, our rate is per sec
     const newToken = (deltaTime * rate) / 1000;
+
+    //min because if rate it too high and user is low, the tokens may go beyond capacity and we are calculating tokens and users on a decrementing basis
     tokens = Math.min(capacity, tokens + newToken);
 
+    //if tokens left in bucket decrement, if all used up, limit the users
     if (tokens < 1) return false;
     else {
       tokens -= 1;
--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const TokenBucket = require("./TokenBucket");
+const FixedWindow = require("./FixedWindow");
 const PORT = process.env.PORT || 3000;
 
 const app = express();
@@ -9,15 +10,19 @@ app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
 
+const handleTokens = TokenBucket(25, 35); //25 tokens/sec and max capacity of 35
+const handleWindow = FixedWindow(15, 1000); // 15 requests per second
+
 //Landing page
 app.get("/", (req, res) => {
   if (handleTokens()) res.send("Welcome Page");
-  else res.status("429").send("L");
+  else res.status("429").send("Bucket empty");
 });
 
 //Confirm status page
 app.get("/status", (req, res) => {
-  res.send("Working!");
+  if (handleWindow()) res.send("Working!");
+  else res.status("429").send("Window overloaded");
 });
 
 //Limited endpoint
@@ -30,5 +35,3 @@ app.get("/limited", (req, res) => {
 app.get("/unlimited", (req, res) => {
   res.send("Unlimited!");
 });
-
-const handleTokens = TokenBucket(25, 35);
