8c70a8cd5553a08d0e9ddd96dfea75bc53f70e4e
haarish04 <haarish.anandan@gmail.com>
2024-05-29 23:46:47 +0530

    Updated sliding window, initially wrong

--- src/SlidingWindow.js
+++ src/SlidingWindow.js
@@ -1,14 +1,2 @@
-class SlidingWindow {
-  constructor(window, limit) {
-    this.window = window;
-    this.limit = limit;
-    this.entries = {};
-  }
-
-  getCurrentWindow() {
-    let currTime = Math.floor(Date.now() / 1000);
-    let offset = currTime % this.window;
-    return [currTime + offset, offset];
-  }
-}
+class SlidingWindow {}
 module.exports = SlidingWindow;
