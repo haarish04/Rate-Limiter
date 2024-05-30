0e68f592e59801d160aefd477f5ef669b3efe2b0
haarish04 <haarish.anandan@gmail.com>
2024-05-30 15:32:03 +0530

    Implemented sliding window

--- src/SlidingWindow.js
+++ src/SlidingWindow.js
@@ -1,2 +1,34 @@
-class SlidingWindow {}
+class SlidingWindow {
+  constructor(limit, windowSize, status) {
+    this.limit = limit;
+    this.windowSize = windowSize;
+    this.lastWindow = Date.now();
+    this.prevCount = 0;
+    this.count = 1;
+  }
+
+  handle() {
+    //if new request falls in next window, push the window boundary by the size of the window
+    //Prev count becomes current count
+    //Current count becomes 0, start new window
+    if (Date.now() - this.lastWindow > this.windowSize) {
+      this.now = Date.now();
+      this.prevCount = this.count;
+      this.count = 0;
+      this.lastWindow = this.lastWindow + this.windowSize;
+    }
+
+    //The weighted count will be considered now, suppose the new req comes in x secs after the window,
+    //we consider the previous window and count the requests from x secs before window started
+    //By doing this we consider the window while crossing the boundary as well
+    estimatedCount =
+      this.prevCount *
+        ((this.windowSize - (this.now - this.lastWindow)) / this.windowSize) +
+      this.count;
+    if (estimatedCount > this.limit) return false;
+
+    this.count++;
+    return true;
+  }
+}
 module.exports = SlidingWindow;
