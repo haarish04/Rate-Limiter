691681a1d3df68990c20a41a3a5305ba9d283625
haarish04 <haarish.anandan@gmail.com>
2024-06-01 01:04:27 +0530

    Refactored Token bucket

--- src/TokenBucket.js
+++ src/TokenBucket.js
@@ -1,25 +1,27 @@
-function TokenBucket(rate, capacity) {
-  let tokens = capacity;
-  let lastCheck = Date.now();
+class TokenBucket {
+  constructor(rate, max) {
+    this.rate = rate;
+    this.max = max;
+    this.lastCheck = Date.now();
+    this.tokens = max;
+  }
 
-  return function handle() {
+  handle() {
     const now = Date.now();
-    const deltaTime = now - lastCheck;
-    lastCheck = now;
+    const elapsed = this.lastCheck - now;
 
     //deltatime will give the secs elapsed * rate will give the number of tokens to be replenished, divided by 1000 as 1000 is one sec, our rate is per sec
-    const newToken = (deltaTime * rate) / 1000;
+    const newToken = (elapsed * rate) / 1000;
 
     //min because if rate it too high and user is low, the tokens may go beyond capacity and we are calculating tokens and users on a decrementing basis
-    tokens = Math.min(capacity, tokens + newToken);
+    tokens = Math.min(this.max, tokens + newToken);
 
     //if tokens left in bucket decrement, if all used up, limit the users
-    if (tokens < 1) return false;
-    else {
-      tokens -= 1;
-      return true;
-    }
-  };
+    if (tokens <= 0) return false;
+
+    tokens -= 1;
+    return true;
+  }
 }
 
 module.exports = TokenBucket;
